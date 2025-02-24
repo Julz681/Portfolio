e5568fe9551be1c830d3381adac4c710da6ded5b
Tobias <tobias.froehler24@gmail.com>
2025-02-24 11:49:32 +0100

    integrated header sidebar plus styling of these

---
+++ js/board.js
@@ -0,0 +1,28 @@
+async function init() {
+    await includeHTML();
+}
+
+async function includeHTML() {
+    let sidebarRef = document.getElementById("sidebarRef");
+    let headerRef = document.getElementById('headerRef');
+    let sidebarRefFile = sidebarRef.getAttribute("w3-include-html");
+    let headerRefFile = headerRef.getAttribute("w3-include-html");
+    await fetchFile(sidebarRef, sidebarRefFile)
+    await fetchFile(headerRef, headerRefFile)
+}
+
+async function fetchFile(element, file) {
+    if (!file) return;
+    try {
+        const response = await fetch(file);
+        if (!response.ok) {
+            throw new Error("Page not found.");
+        }
+        element.innerHTML = await response.text();
+    } catch (error) {
+        element.innerHTML = error.message;
+    }
+    finally {
+        element.removeAttribute("w3-include-html");
+    }
+}
