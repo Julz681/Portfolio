fc6391604f7de2d8e6df972af83d772bad56c605
19bernadette99 <bernadette.poeltl99@gmail.com>
2025-02-26 16:37:39 +0100

    fixed header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+// This function is used for the animation at the beginning.
+
 document.querySelector('.start').addEventListener('animationend', function() {
     this.classList.add('finished');
 });
 
+
+//The event listener is for the help button in the header, making it disappear when on the help HTML page and reappear when leaving it.
 document.addEventListener("DOMContentLoaded", () => {
       const helpButton = document.querySelector(".help-btn");
       if (helpButton) {
@@ -9,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
   });
 
+  //This function ensures that the arrow always brings the user back to the page they were on before clicking the help button.
   function goBack() {
     window.history.back();
 }
