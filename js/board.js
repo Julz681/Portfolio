4ca3a5bfc89e21424c2d5f25eee091a5a5fe6050
Tobias <tobias.froehler24@gmail.com>
2025-03-09 19:27:12 +0100

    integrated general frame into add task + start of add task design with inputs

--- js/board.js
+++ js/board.js
@@ -1,14 +1,39 @@
+// async function init() {
+//   await includeHTML();
+// }
+
+// async function includeHTML() {
+//   let sidebarRef = document.getElementById("sidebarRef");
+//   let headerRef = document.getElementById("headerRef");
+//   let sidebarRefFile = sidebarRef.getAttribute("w3-include-html");
+//   let headerRefFile = headerRef.getAttribute("w3-include-html");
+//   await fetchFile(sidebarRef, sidebarRefFile);
+//   await fetchFile(headerRef, headerRefFile);
+// }
+
+// async function fetchFile(element, file) {
+//   if (!file) return;
+//   try {
+//     const response = await fetch(file);
+//     if (!response.ok) {
+//       throw new Error("Page not found.");
+//     }
+//     element.innerHTML = await response.text();
+//   } catch (error) {
+//     element.innerHTML = error.message;
+//   } finally {
+//     element.removeAttribute("w3-include-html");
+//   }
+// }
+
 async function init() {
   await includeHTML();
 }
 
 async function includeHTML() {
-  let sidebarRef = document.getElementById("sidebarRef");
-  let headerRef = document.getElementById("headerRef");
-  let sidebarRefFile = sidebarRef.getAttribute("w3-include-html");
-  let headerRefFile = headerRef.getAttribute("w3-include-html");
-  await fetchFile(sidebarRef, sidebarRefFile);
-  await fetchFile(headerRef, headerRefFile);
+  let generalFrameRef = document.getElementById("general-frame");
+  let generalFrameFile = generalFrameRef.getAttribute("w3-include-html");
+  await fetchFile(generalFrameRef, generalFrameFile);
 }
 
 async function fetchFile(element, file) {
--- up.bat
+++ up.bat
@@ -1,6 +1,6 @@
 git pull
 git add .
-git commit -m ""
+git commit -m "%*"
 git push
 
 git pull origin main
